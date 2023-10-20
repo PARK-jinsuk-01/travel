b0fb0fd1b2bfb0a3b2b6c2f32106a31310e5c3f0
quack <rudals23yoo@naver.com>
2023-10-20 19:22:21 +0900

    signup-이메일인증구현

--- src/main/java/com/example/travel/controller/MemberController.java
+++ src/main/java/com/example/travel/controller/MemberController.java
@@ -1,6 +1,6 @@
 package com.example.travel.controller;
 
-import java.util.Optional;
+import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
@@ -13,8 +13,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.travel.model.Member;
@@ -30,30 +29,6 @@ public class MemberController {
     @Autowired
     HttpSession session;
 
-    // @GetMapping("/signin")
-    // public String signinPage() {
-    // return "sign/signin";
-    // }
-
-    // @PostMapping("/signin")
-    // public String signinPost(@ModelAttribute Member member, Model model) {
-    // Member dbUser = memberRepository.findByEmail(member.getEmail());
-
-    // if (dbUser != null) {
-    // if (passwordEncoder.matches(member.getPw(), dbUser.getPw())) {
-    // // 로그인 성공
-    // session.setAttribute("user_id", dbUser.getId());
-    // session.setAttribute("user_email", dbUser.getEmail());
-    // return "redirect:/";
-    // } else {
-    // model.addAttribute("error", "비밀번호가 일치하지 않습니다.");
-    // }
-    // } else {
-    // model.addAttribute("error", "사용자가 존재하지 않습니다.");
-    // }
-    // return "sign/signin";
-    // }
-
     @GetMapping("/signup")
     public String signup() {
         return "sign/signup";
@@ -62,38 +37,59 @@ public class MemberController {
     @PostMapping("/signup")
     @ResponseBody
     @Transactional
-    public String signupPost(@ModelAttribute Member member, BindingResult bindingResult) {
+    public String signupPost(@RequestBody Map<String, Object>data, BindingResult bindingResult, HttpSession session) {
         // 빈 필드 검사
-        if (member.getEmail() == null || member.getEmail().isEmpty() ||
-            member.getPw() == null || member.getPw().isEmpty() ||
-            member.getPhone() == null || member.getPhone().isEmpty()) {
+        String email =(String) data.get("email");
+        String pw =(String) data.get("pw");
+        String name =(String) data.get("name");
+        String phone =(String) data.get("phone");
+        String yy =(String) data.get("yy");
+        String mm =(String) data.get("mm");
+        String dd =(String) data.get("dd");
+        
+
+        System.out.println(data.get("name"));
+        if (email == null || email.isEmpty() ||
+           pw == null || pw.isEmpty() ||
+           phone == null || phone.isEmpty()) {
             return "모든 필드를 작성해주세요.";
         }
-       
-        String birth = member.getYy() + "-" + member.getMm() + "-" + member.getDd();
-        member.setBirth(birth);
-        System.out.println(birth);
 
-        Member emailCheck = memberRepository.findByEmail(member.getEmail());
+        String birth = yy + "-" + mm + "-" + dd;
+        
+
+        
+
+        // 세션 데이터와 입력 데이터 비교
+       
+        Member emailCheck = memberRepository.findByEmail(email);
         if (emailCheck != null) {
             return "이미 가입된 이메일입니다.";
         }
-        Member phoneCheck = memberRepository.findByPhone(member.getPhone());
+        Member phoneCheck = memberRepository.findByPhone(phone);
         if (phoneCheck != null) {
             return "중복된 핸드폰 번호입니다.";
         }
 
-        String rawPassword = member.getPw();
+        String rawPassword = pw;
         String encodedPassword = passwordEncoder.encode(rawPassword);
-        member.setPw(encodedPassword);
+        
+        Member member = new Member();
+        member.setEmail(email);
+        member.setPw(pw);
+        member.setName(name);
+        member.setPhone(phone);
+        member.setBirth(birth);
 
+        // Member 엔터티를 데이터베이스에 저장
         memberRepository.save(member);
+
         return "가입이 완료되었습니다.";
     }
 
-    @RequestMapping("/checkEmailAvailability")
+    @GetMapping("/checkEmailAvailability")
     @ResponseBody
-    public String checkEmailAvailability(@RequestParam String email) {
+    public String checkEmailAvailability(String email) {
         Member emailCheck = memberRepository.findByEmail(email);
         if (emailCheck != null) {
             return "가입불가";
